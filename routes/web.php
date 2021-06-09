199a42de0e75d4d589fe619cad16d7cddf360a05
powlam <palbandoz+github@gmail.com>
2021-06-10 00:28:20 +0200

    breeze (authentication kit)

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Route::group(['middleware' => 'language'], function () {
         return view('welcome');
     });
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+
+    require __DIR__.'/auth.php';
+
 });

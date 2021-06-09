60e32b1ea3d43ff53200fad4972f168ff3e21cb2
powlam <palbandoz+github@gmail.com>
2021-06-09 23:50:50 +0200

    laravel-language (language switcher)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'language'], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
 });
